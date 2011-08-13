6d4a15f199417325c12157a57ca0a672591f14d1
Fabien Franzen <info@atelierfabien.be>
2011-08-13 20:13:27 +0200

    Google currency rate fix

--- lib/exchange_services/google.rb
+++ lib/exchange_services/google.rb
@@ -6,11 +6,11 @@ class ExchangeService::Google < ExchangeService::Base
     date_req = Time.now
     Currency.all.map do |currency|
       unless currency.char_code == @base_currency_code
-        url = "http://www.google.com/ig/calculator?hl=en&q=1#{ currency.char_code }%3D%3F#{ @base_currency_code }"
+        url = "http://www.google.com/ig/calculator?hl=en&q=1#{ @base_currency_code }%3D%3F#{ currency.char_code }"
         data = JSON.parse(open(url).read.gsub(/lhs:|rhs:|error:|icc:/){ |x| "\"#{x[0..-2]}\":"})
         if data["error"].blank?
           value = BigDecimal(data["rhs"].split(' ')[0])
-          currency.update_attributes(:value => currency.nominal/value.to_f, :date_req => date_req)
+          currency.update_attributes(:value => value, :date_req => date_req)
         end
       end
     end.any?
