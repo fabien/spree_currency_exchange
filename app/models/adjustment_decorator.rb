aa561b3459e42609481882195d8adf61d2a2e5b2
Fabien Franzen <info@atelierfabien.be>
2011-11-09 23:21:27 +0100

    Handle completed order's adjustments (unchangeable)

--- app/models/adjustment_decorator.rb
+++ app/models/adjustment_decorator.rb
@@ -1,6 +1,25 @@
 Adjustment.class_eval do
+
+  def amount
+    if associated_with_completed_order?
+      read_attribute(:amount)
+    else
+      Currency.convert_to_current(read_attribute(:amount))
+    end
+  end
   
-  extend CurrencyExchange
-  currency_exchange :amount
+  def amount=(value)
+    if associated_with_completed_order?
+      write_attribute(:amount, value)
+    else
+      write_attribute(:amount, Currency.convert_from_current(value))
+    end
+  end
+  
+  protected
+  
+  def associated_with_completed_order?
+    order && order.state == 'complete'
+  end
   
 end
